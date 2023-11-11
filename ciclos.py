3056a79ac00e74fbb8b601c0eb5bc2da75839a69
David1193 <eliandavidax100@gmail.com>
2023-11-10 19:38:50 -0500

    firts comment

---
+++ ciclos.py
@@ -0,0 +1,127 @@
+# num  =int (input('ingrese un numero: '))
+
+
+
+# if num % 2 == 0:
+#     print(num,'es par')
+# else:
+#     print(num,'es impar')
+
+
+
+
+
+# edad =int(input('ingrese su edad :'))
+ 
+# if edad >= 18 and edad <=65 :
+#     print('eres  adulto')
+# elif edad >=65:
+#     print('eres adulto mayor')
+
+# else:
+#     print('eres menor de edad')
+
+
+
+
+
+# notas = int (input('ingrese puntuacion :'))
+
+# if notas >= 90 and notas <=100 :
+#     print("'A'=",'sobresaliente')
+
+# elif notas >=80 and notas <=89 :
+#     print("'B'=",'bueno')
+# elif notas >=70 and notas <=79 :
+#     print("'C'=",'satisfactorio')
+# elif notas >= 60 and notas <=69:
+#     print("'D'=",'aceptable')
+# else:
+#     print ( "'f'=",'reprobado' )
+
+
+
+
+# contador = 0
+
+# while contador < 5:
+#     print(f"Valor del contador: {contador}")
+#     contador += 2
+
+# asc = 0
+# des = 11
+
+# while asc < 10:
+#     asc += 1
+#     des -= 1
+#     print(asc ,'//', des)
+
+
+
+
+
+
+# import random
+
+# aleatoreo = random.randint(1, 100)
+
+# adivina = 0
+
+# while adivina != aleatoreo:
+#     if adivina == 0:
+#         print('Inicia el Juego')
+#     elif adivina < aleatoreo:
+#         print('demasiado bajo')
+#     else:
+#         print('demasiado alto')
+#     adivina = int(input('Ingresa un numero'))
+# print('Haz ganado!')
+
+
+
+# 1
+
+# num = int(input('ingresa un numero: '))
+
+# for i in range(1,11):
+#     mult= num*i
+#     print(num ,'x', i, '=', mult )
+
+
+#3 patron de asteriscos:
+# n = 5
+
+# for i in range(1, n + 1):
+#     for j in range(i):
+#         print('*  ', end='')
+#     print()
+
+
+
+
+
+#1:
+
+# suma = 0
+# n =1
+# while n !=0:
+#     n = int(input("ingresa un numero: "))
+#     if n != 0:
+#         if n %2 == 0:
+#             suma = suma + n
+# print("la suma de los numeros pares son: ", suma )            
+
+#2 
+ 
+
+# pre = int (input("precio: "))
+# cant =int(input("cantidad:"))
+# sub = pre * cant
+# desc = sub * 0.10
+# tot = sub - desc
+# print("subtotal: ",sub)
+# print("descuento: ", desc)
+# print("total a pagar:",tot)
+
+
+
